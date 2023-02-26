954f8b99d0cdbc1969be091d3e7eaeaf8ab330b7
Noronha1612 <inc.691@gmail.com>
2023-02-25 21:59:19 -0300

    Create user card display component

--- src/pages/Home/components/UserCard/index.tsx
+++ src/pages/Home/components/UserCard/index.tsx
@@ -1,26 +1,31 @@
 
+import { useUser } from '@/hooks/useUser'
 import { FaBuilding, FaGithub, FaShareSquare, FaUserFriends } from 'react-icons/fa'
 
 import * as S from './styles'
 
 export const UserCard = () => {
+	const { user, loading } = useUser()
+
+	if (loading) return <></>
+
 	return (
 		<S.Container>
-			<S.AvatarImage src='https://avatars.githubusercontent.com/u/56375499?v=4' />
+			<S.AvatarImage src={user?.avatar_url} alt="Avatar" />
 
 			<S.Content>
 				<div className="flex-sp-btw">
-					<h1>Gabriel Noronha</h1>
+					<h1>{user?.name}</h1>
 
-					<a href="#">github <FaShareSquare size={12} /></a>
+					<a href={user?.html_url}>github <FaShareSquare size={12} /></a>
 				</div>
 
-				<p>Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.</p>
+				<p>{user?.bio}</p>
 
 				<footer>
-					<span><FaGithub size={18} /> Noronha1612</span>
-					<span><FaBuilding size={18} /> Banco Inter</span>
-					<span><FaUserFriends size={18} /> 20 seguidores</span>
+					<span><FaGithub size={18} /> {user?.login}</span>
+					<span><FaBuilding size={18} /> {user?.company}</span>
+					<span><FaUserFriends size={18} /> {user?.followers} seguidores</span>
 				</footer>
 			</S.Content>
 		</S.Container>
